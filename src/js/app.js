a4cae5231688c4875d94645e9f702af14233553e
Griffin Byron <puginabox@gmail.com>
2015-06-25 14:21:19 -0400

    playing around with fill attribute

--- src/js/app.js
+++ src/js/app.js
@@ -12,9 +12,8 @@ var bgdColor = '#268BD2',
 var w = 400, // width of svg container
     h = 500, // height of svg container
     padding = 2,
-    dataset = [35, 13, 220, 119, 377, 27, 33],
-    svg1 = d3.select('#deThree')
-    .append('svg')
+    dataset = [35, 413, 100, 82, 220, 119, 377, 27, 33],
+    svg1 = d3.select('#deThree').append('svg')
     .attr('width', w)
     .attr('height', h)
     .style({
@@ -26,6 +25,9 @@ var w = 400, // width of svg container
 svg1.selectAll('rect')
     .data(dataset).enter()
     .append('rect')
+    .attr('fill', function(d) {
+        return 'rgb(0, 0, ' + (d * 3) + ' )';
+    })
     .attr('x', function(d, i) {
         return (i * (w / dataset.length));
     })
