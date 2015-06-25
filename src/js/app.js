0964ed3c51ab9e9e3f661a5696bc31b978043f16
Crunch <crunch@leviathan.home>
2015-06-25 12:58:25 -0400

    app.js in wrong place

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,11 @@ d3.select('body')
     .append('rect')
     .attr('width', 20)
     .attr('height', 100)
-    .style('background', 'green');
+    .style('background', '#EEE8D5');
+
+//------Color Library
+var bgdColor = '#268BD2',
+    txtColor = '#EEE8D5';
 
 
 var w = 200,
@@ -27,8 +31,6 @@ svg1.selectAll('rect')
     .attr('y', 0)
     .attr('width', 20)
     .attr('height', 100);
-
-console.log('hello');
 
 // .attr('x', function(d, i) {
 //     return (i * (w / dataset.length));
