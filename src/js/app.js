d208e1d4cae47d58c006e9e6b96ceaebb163320e
Griffin Byron <puginabox@gmail.com>
2015-06-25 13:23:32 -0400

    Adding Bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,6 @@
  *
  **/
 
-d3.select('body')
-    .append('rect')
-    .attr('width', 20)
-    .attr('height', 100)
-    .style('background', '#EEE8D5');
-
 //------Color Library
 var bgdColor = '#268BD2',
     txtColor = '#EEE8D5';
@@ -27,14 +21,11 @@ var w = 200,
 svg1.selectAll('rect')
     .data(dataset).enter()
     .append('rect')
-    .attr('x', 0)
-    .attr('y', 0)
     .attr('width', 20)
-    .attr('height', 100);
-
-// .attr('x', function(d, i) {
-//     return (i * (w / dataset.length));
-// })
-// .attr('y', 0)
-// .attr('width', w / dataset.length - padding)
-// .attr('height', h);
+    .attr('height', 100)
+    .attr('x', function(d, i) {
+        return (i * (w / dataset.length));
+    })
+    .attr('y', 0)
+    .attr('width', w / dataset.length - padding)
+    .attr('height', h);
