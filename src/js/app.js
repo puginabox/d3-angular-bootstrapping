58d9d3cd8897cd4975882d4a5bcf36731bd699dd
Griffin Byron <puginabox@gmail.com>
2015-06-25 13:57:35 -0400

    d3 upside down hack

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,11 @@ var bgdColor = '#268BD2',
     txtColor = '#EEE8D5';
 
 
-var w = 200,
+var w = 400,
     h = 100,
     padding = 2,
-    dataset = [5, 13, 20, 19, 7],
-    svg1 = d3.select('body')
+    dataset = [5, 13, 220, 119, 77, 27, 33],
+    svg1 = d3.select('#deThree')
     .append('svg')
     .attr('width', w)
     .attr('height', h);
@@ -21,11 +21,13 @@ var w = 200,
 svg1.selectAll('rect')
     .data(dataset).enter()
     .append('rect')
-    .attr('width', 20)
-    .attr('height', 100)
     .attr('x', function(d, i) {
         return (i * (w / dataset.length));
     })
-    .attr('y', 0)
+    .attr('y', function(d) {
+        return h - d; //upside down hack
+    })
     .attr('width', w / dataset.length - padding)
-    .attr('height', h);
+    .attr('height', function(d) {
+        return d;
+    });
